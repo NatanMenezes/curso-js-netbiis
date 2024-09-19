ca6eb19bf0fc07d4f9851b56f4b16f3805fb8343
natanmenezes <natanmenezes31@gmail.com>
2024-09-19 20:33:56 -0300

    Unidade 5 - parte 1

---
+++ Curso 1/Unidade 5/condicionais_compostas.js
@@ -0,0 +1,7 @@
+let numero = 10;
+
+if (numero >= 10) {
+    console.log('O número é maior ou igual a 10');
+} else {
+    console.log('O número é menor que 10');
+}
---
+++ Curso 1/Unidade 5/condicionais_simples.js
@@ -0,0 +1,9 @@
+let numero = 9;
+
+if (numero >= 10) {
+    console.log('O número é maior ou igual a 10');
+}
+
+if (numero < 10) {
+    console.log('O número é menor que 10');
+}
