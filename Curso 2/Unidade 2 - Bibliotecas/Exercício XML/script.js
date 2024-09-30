39d79d3e4fa9d185ff82775a93cc4c881dadad44
natanmenezes <natanmenezes31@gmail.com>
2024-09-30 18:58:58 -0300

    Unidade 2.2 (Base)

---
+++ Curso 2/Unidade 2 - Bibliotecas/Exercício XML/script.js
@@ -0,0 +1,34 @@
+const xmlString = /* xml */ `
+    <frota>
+      <informacoes>
+        <nomeEmpresa>Transportes Rapidez</nomeEmpresa>
+        <sede>
+          <rua>Rua da Logística, 456</rua>
+          <cidade>Rio de Janeiro</cidade>
+          <estado>RJ</estado>
+          <cep>20000-000</cep>
+        </sede>
+        <telefone>(21) 9876-5432</telefone>
+      </informacoes>
+      <veiculos>
+        <veiculo>
+          <modelo>Mercedes-Benz Sprinter</modelo>
+          <ano>2020</ano>
+          <placa>XYZ-1234</placa>
+          <disponivel>true</disponivel>
+        </veiculo>
+        <veiculo>
+          <modelo>Volkswagen Delivery</modelo>
+          <ano>2018</ano>
+          <placa>ABC-5678</placa>
+          <disponivel>false</disponivel>
+        </veiculo>
+        <veiculo>
+          <modelo>Ford Cargo</modelo>
+          <ano>2021</ano>
+          <placa>DEF-9101</placa>
+          <disponivel>true</disponivel>
+        </veiculo>
+      </veiculos>
+    </frota>
+`;
