7bf8310c524b76bcf60cf6ee97d816cc1c958694
Pedro Klimaitis <pedro.klimaitis@gmail.com>
2024-10-14 16:06:49 -0300

    push

---
+++ curso js netbiis/Curso 2/Unidade 3 - Modulos/strings_client.mjs
@@ -0,0 +1,6 @@
+import {contarVogais, InverterString, verificarTamanho} from './operacoes_strings.mjs';
+
+const str = 'JavaScript é na DevMedia';
+
+console.log(contarVogais(str));
+console.log(InverterString(str));
