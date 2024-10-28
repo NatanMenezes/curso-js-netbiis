c950788a6d3650e8321dc2c5970d059ff9378991
Pedro Klimaitis <pedro.klimaitis@gmail.com>
2024-10-28 20:05:58 -0300

    npm install joi

--- Curso 4/Unidade 2 - Express/helloworld/src/router/AlunosRouter.js
+++ Curso 4/Unidade 2 - Express/helloworld/src/router/AlunosRouter.js
@@ -34,3 +34,4 @@ router.delete('/', (req, res) => {
 })
 
 export default router;
+
--- Curso 4/Unidade 2 - Express/helloworld/src/router/index.js
+++ Curso 4/Unidade 2 - Express/helloworld/src/router/index.js
@@ -1,12 +1,16 @@
 import AlunosRouter from "./AlunosRouter.js";
+import Joi from 'joi';
 import { Router } from "express";
 
 const router = Router();
 
 router.get('/', (req, res) => {
+    let teste = Joi.assert(5, Joi.number());
+    console.log(teste);
     res.send('Hello World');
 });
 
 router.use('/alunos', AlunosRouter);
 
 export default router;
+
